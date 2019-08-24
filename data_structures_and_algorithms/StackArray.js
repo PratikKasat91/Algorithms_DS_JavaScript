6759a824e1cdee74a57be5e0f84f53221c1bb7bb
PratikKasat91 <prk3011@gmail.com>
2019-08-24 17:35:45 -0400

    add challenges and stack array

---
+++ data_structures_and_algorithms/StackArray.js
@@ -0,0 +1,69 @@
+/**
+ * Stack Data Structure Using Arrays.
+ */
+class StackArray {
+  constructor() {
+    this.a = [];
+    this.n = 0;
+  }
+
+  isEmpty() {
+    return this.n === 0;
+  }
+  
+  size() {
+    return this.n;
+  }
+
+  push(item) {
+    this.a[this.n] = item; 
+    this.n++;
+  }
+
+  pop() {
+    if (this.isEmpty()) {
+      throw new Error('Stack underflow');
+    }
+
+    const item = this.a[this.n-1];
+    this.a[this.n-1] = null;
+    this.n--;  
+    return item;
+  }
+
+  peek() {
+    return this.a[this.n-1];
+  }
+
+  [Symbol.iterator]() {
+    const a = this.a;
+    let index = this.n-1;
+
+    return {
+      next() {
+        if (index < 0) {
+          return {done: true}; // end of iteration
+        }
+
+        return {
+          value: a[index--],
+          done: false,
+        };
+      }
+    }
+  }
+}
+
+
+(function run() {
+  const stack = new StackArray();
+  stack.push(1);
+  stack.push(2);
+  stack.push(3);
+  stack.push(4);
+  stack.push(5);
+  console.log(stack.size(), ...stack);
+  stack.pop();
+  stack.pop();
+  console.log(stack.size(), ...stack);
+})();
